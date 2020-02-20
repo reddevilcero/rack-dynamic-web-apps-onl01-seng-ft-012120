ca82be4af4ad169c400ba777c98727971f7f754c
reddevilcero <reddevil_cero@hotmail.com>
2020-02-20 09:31:40 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,20 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write self.slot_machine
     resp.finish
+  end
+
+  def slot_machine
+    num1 = Kernel.rand(1..2)
+    num2 = Kernel.rand(1..2)
+    num3 = Kernel.rand(1..2)
+  
+    if num1 == num2 && num2 == num3
+      "#{num1} - #{num2} - #{num3} 'You Win.'"
+    else
+      "#{num1} - #{num2} - #{num3} 'You Lose.'"
+    end
   end
 
 end
